e6f9b5280adefdf763668d4229b1fd90a94b38a3
dev-madilyn <lollin0@naver.com>
2022-07-06 18:54:45 +0900

    Add: Mument Color

---
+++ MUMENT/MUMENT/Extensions/UIColor+.swift
@@ -0,0 +1,72 @@
+//
+//  UIColor+.swift
+//  MUMENT
+//
+//  Created by madilyn on 2022/07/06.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var Black1: UIColor {
+    return UIColor(white: 34.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Black2: UIColor {
+    return UIColor(white: 75.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var White: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Gray1: UIColor {
+    return UIColor(white: 131.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Gray3: UIColor {
+    return UIColor(white: 216.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Gray4: UIColor {
+    return UIColor(white: 232.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Purple1: UIColor {
+    return UIColor(red: 135.0 / 255.0, green: 44.0 / 255.0, blue: 254.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Blue1: UIColor {
+    return UIColor(red: 42.0 / 255.0, green: 201.0 / 255.0, blue: 251.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Purple2: UIColor {
+    return UIColor(red: 234.0 / 255.0, green: 227.0 / 255.0, blue: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Blue2: UIColor {
+    return UIColor(red: 184.0 / 255.0, green: 238.0 / 255.0, blue: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Deeppurple: UIColor {
+    return UIColor(red: 98.0 / 255.0, green: 0.0, blue: 225.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Bgwhite: UIColor {
+    return UIColor(white: 250.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Gray2: UIColor {
+    return UIColor(white: 182.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Gray5: UIColor {
+    return UIColor(white: 245.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var Blue3: UIColor {
+    return UIColor(red: 228.0 / 255.0, green: 249.0 / 255.0, blue: 1.0, alpha: 1.0)
+  }
+
+}
