5e5f0c1e1de31ef6796979e5909f9c6697d469d3
dev-madilyn <lollin0@naver.com>
2022-07-07 01:51:26 +0900

    Rename: 컬러 앞에 m- 붙이기

--- MUMENT/MUMENT/Extensions/UIColor+.swift
+++ MUMENT/MUMENT/Extensions/UIColor+.swift
@@ -9,63 +9,63 @@ import UIKit
 
 extension UIColor {
 
-  @nonobjc class var Black1: UIColor {
+  @nonobjc class var mBlack1: UIColor {
     return UIColor(white: 34.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var Black2: UIColor {
+  @nonobjc class var mBlack2: UIColor {
     return UIColor(white: 75.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var White: UIColor {
+  @nonobjc class var mWhite: UIColor {
     return UIColor(white: 1.0, alpha: 1.0)
   }
 
-  @nonobjc class var Gray1: UIColor {
+  @nonobjc class var mGray1: UIColor {
     return UIColor(white: 131.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var Gray3: UIColor {
+  @nonobjc class var mGray3: UIColor {
     return UIColor(white: 216.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var Gray4: UIColor {
+  @nonobjc class var mGray4: UIColor {
     return UIColor(white: 232.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var Purple1: UIColor {
+  @nonobjc class var mPurple1: UIColor {
     return UIColor(red: 135.0 / 255.0, green: 44.0 / 255.0, blue: 254.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var Blue1: UIColor {
+  @nonobjc class var mBlue1: UIColor {
     return UIColor(red: 42.0 / 255.0, green: 201.0 / 255.0, blue: 251.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var Purple2: UIColor {
+  @nonobjc class var mPurple2: UIColor {
     return UIColor(red: 234.0 / 255.0, green: 227.0 / 255.0, blue: 1.0, alpha: 1.0)
   }
 
-  @nonobjc class var Blue2: UIColor {
+  @nonobjc class var mBlue2: UIColor {
     return UIColor(red: 184.0 / 255.0, green: 238.0 / 255.0, blue: 1.0, alpha: 1.0)
   }
 
-  @nonobjc class var Deeppurple: UIColor {
+  @nonobjc class var mDeeppurple: UIColor {
     return UIColor(red: 98.0 / 255.0, green: 0.0, blue: 225.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var Bgwhite: UIColor {
+  @nonobjc class var mBgwhite: UIColor {
     return UIColor(white: 250.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var Gray2: UIColor {
+  @nonobjc class var mGray2: UIColor {
     return UIColor(white: 182.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var Gray5: UIColor {
+  @nonobjc class var mGray5: UIColor {
     return UIColor(white: 245.0 / 255.0, alpha: 1.0)
   }
 
-  @nonobjc class var Blue3: UIColor {
+  @nonobjc class var mBlue3: UIColor {
     return UIColor(red: 228.0 / 255.0, green: 249.0 / 255.0, blue: 1.0, alpha: 1.0)
   }
 
