33ac47f4a3542dfb5931e8c35be78531212b753c
dev-madilyn <lollin0@naver.com>
2022-07-07 02:17:36 +0900

    Feat: 진입 뷰 TabBarController로 진입하도록 설정 #1

---
+++ MUMENT/MUMENT/Storage/StorageVC.swift
@@ -0,0 +1,15 @@
+//
+//  StorageVC.swift
+//  MUMENT
+//
+//  Created by madilyn on 2022/07/06.
+//
+
+import UIKit
+
+class StorageVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
