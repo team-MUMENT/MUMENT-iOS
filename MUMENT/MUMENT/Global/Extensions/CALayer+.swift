1e0016be325763401f1475423c2f971708abf6aa
dev-madilyn <lollin0@naver.com>
2022-07-08 13:47:25 +0900

    Add: CALayer+

---
+++ MUMENT/MUMENT/Global/Extensions/CALayer+.swift
@@ -0,0 +1,21 @@
+//
+//  CALayer+.swift
+//  MUMENT
+//
+//  Created by madilyn on 2022/07/08.
+//
+
+import UIKit
+
+extension CALayer {
+    func applyShadow(color: UIColor = .black,
+                     alpha: Float = 0.5,
+                     x: CGFloat = 0,
+                     y: CGFloat = 2,
+                     blur: CGFloat = 4) {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+    }
+}
