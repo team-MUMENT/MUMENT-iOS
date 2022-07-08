8660bc74905cd8cdfe09ca2a2e8d3e9e0da0c744
dev-madilyn <lollin0@naver.com>
2022-07-08 20:00:12 +0900

    chore: Semibold 삭제 #8

--- MUMENT/MUMENT/Global/Extensions/UIFont+.swift
+++ MUMENT/MUMENT/Global/Extensions/UIFont+.swift
@@ -8,49 +8,45 @@
 import UIKit
 
 extension UIFont {
-
-  class var mumentH1Sb25: UIFont {
-    return UIFont(name: "NotoSans-SemiBold", size: 25.0)!
-  }
-
-  class var mumentH2B18: UIFont {
-    return UIFont(name: "NotoSans-Bold", size: 18.0)!
-  }
-
-  class var mumentH3Sb18: UIFont {
-    return UIFont(name: "NotoSans-SemiBold", size: 18.0)!
-  }
-
-  class var mumentH4Sb16: UIFont {
-    return UIFont(name: "NotoSans-SemiBold", size: 16.0)!
-  }
-
-  class var mumentH5M16: UIFont {
-    return UIFont(name: "NotoSans-Medium", size: 16.0)!
-  }
-
-  class var mumentB1B15: UIFont {
-    return UIFont(name: "NotoSans-Bold", size: 15.0)!
-  }
-
-  class var mumentB2B13: UIFont {
-    return UIFont(name: "NotoSans-SemiBold", size: 13.0)!
-  }
-
-  class var mumentB3M13: UIFont {
-    return UIFont(name: "NotoSans-Regular", size: 13.0)!
-  }
-
-  class var mumentB4Sb12: UIFont {
-    return UIFont(name: "NotoSans-SemiBold", size: 12.0)!
-  }
-
-  class var mumentB5M12: UIFont {
-    return UIFont(name: "NotoSans-Medium", size: 12.0)!
-  }
-
-  class var mumentC1R12: UIFont {
-    return UIFont(name: "NotoSans-Regular", size: 12.0)!
-  }
-
+    
+    class var mumentH1B25: UIFont {
+        return UIFont(name: "NotoSans-Bold", size: 25.0)!
+    }
+    
+    class var mumentH2B18: UIFont {
+        return UIFont(name: "NotoSans-Bold", size: 18.0)!
+    }
+    
+    class var mumentH3B16: UIFont {
+        return UIFont(name: "NotoSans-Bold", size: 16.0)!
+    }
+    
+    class var mumentH4M16: UIFont {
+        return UIFont(name: "NotoSans-Medium", size: 16.0)!
+    }
+    
+    class var mumentB1B15: UIFont {
+        return UIFont(name: "NotoSans-Bold", size: 15.0)!
+    }
+    
+    class var mumentB2B13: UIFont {
+        return UIFont(name: "NotoSans-Bold", size: 13.0)!
+    }
+    
+    class var mumentB3M13: UIFont {
+        return UIFont(name: "NotoSans-Regular", size: 13.0)!
+    }
+    
+    class var mumentB4B12: UIFont {
+        return UIFont(name: "NotoSans-Bold", size: 12.0)!
+    }
+    
+    class var mumentB5M12: UIFont {
+        return UIFont(name: "NotoSans-Medium", size: 12.0)!
+    }
+    
+    class var mumentC1R12: UIFont {
+        return UIFont(name: "NotoSans-Regular", size: 12.0)!
+    }
+    
 }
