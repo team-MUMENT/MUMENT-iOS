55ee6e5f1f8b274f3c6f55d28004dcf5dd12f3a9
dev-madilyn <lollin0@naver.com>
2022-07-11 01:33:20 +0900

    Feat: 곡 선택 라벨, 검색 버튼, 처음 들은 곡 라벨 추가 및 레이아웃 설정 #30

--- MUMENT/MUMENT/Sources/Scenes/Write/WriteVC.swift
+++ MUMENT/MUMENT/Sources/Scenes/Write/WriteVC.swift
@@ -22,7 +22,27 @@ class WriteVC: BaseVC {
     private let resetButton = UIButton(type: .system).then {
         $0.setImage(UIImage(named: "mumentReset"), for: .normal)
     }
-    
+    private let selectMusicLabel = UILabel().then {
+        $0.text = "곡을 선택해주세요."
+        $0.font = .mumentB1B15
+        $0.textColor = .mBlack2
+    }
+    private let searchButton = UIButton(type: .system).then {
+        $0.setTitle("곡, 아티스트 검색", for: .normal)
+        $0.setTitleColor(.mGray1, for: .normal)
+//        $0.titleLabel?.font = .mumentB4B14
+        $0.backgroundColor = .mGray5
+        $0.layer.cornerRadius = 10
+        $0.configuration = .plain()
+//        $0.configuration?.image = UIImage(named: "mumentSearch")
+        $0.configuration?.imagePadding = 10
+        $0.contentHorizontalAlignment = .left
+    }
+    private let firstTimeMusicLabel = UILabel().then {
+        $0.text = "처음 들은 곡인가요?"
+        $0.font = .mumentB1B15
+        $0.textColor = .mBlack2
+    }
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +55,7 @@ extension WriteVC {
     private func setLayout() {
         view.addSubviews([writeScrollView])
         writeScrollView.addSubviews([writeContentView])
-        writeContentView.addSubviews([naviView, resetButton])
+        writeContentView.addSubviews([naviView, resetButton, selectMusicLabel, searchButton, firstTimeMusicLabel])
         
         writeScrollView.snp.makeConstraints {
             $0.edges.equalTo(view.safeAreaLayoutGuide)
@@ -55,6 +75,22 @@ extension WriteVC {
             $0.centerY.equalTo(naviView)
             $0.width.height.equalTo(25.adjustedW)
             $0.rightMargin.equalToSuperview().inset(20)
+        }
+        
+        selectMusicLabel.snp.makeConstraints {
+            $0.top.equalTo(naviView.snp.bottom).offset(40)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+        
+        searchButton.snp.makeConstraints {
+            $0.top.equalTo(selectMusicLabel.snp.bottom).offset(16)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(40.adjustedH)
+        }
+        
+        firstTimeMusicLabel.snp.makeConstraints {
+            $0.top.equalTo(searchButton.snp.bottom).offset(50)
+            $0.horizontalEdges.equalToSuperview().inset(20)
         }
     }
 }
