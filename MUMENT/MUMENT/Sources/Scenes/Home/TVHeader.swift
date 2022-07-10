943839f6d2719ab54061fa70a34d9dcdff9b8796
Jimin Kim <0305jimin@gmail.com>
2022-07-10 14:51:15 +0900

    Feat: 검색 뷰로 이동하는 버튼 구현#12

--- MUMENT/MUMENT/Sources/Scenes/Home/TVHeader.swift
+++ MUMENT/MUMENT/Sources/Scenes/Home/TVHeader.swift
@@ -20,6 +20,24 @@ class TVHeader: UIView {
         $0.setImage(UIImage(named: "mumentNoti"), for: .normal)
     }
     
+    lazy var searchButton = UIButton().then{
+//        $0.setImage(UIImage(named: "mumentSearch"), for: .normal)
+//        $0.setBackgroundImage(UIImage(named: "mumentSearchbar"), for: .normal)
+//        $0.setImage(UIImage(named: "mumentSearchbar"), for: .normal)
+        $0.setTitle("뮤멘트를 둘러보세요.", for: .normal)
+        $0.setTitleColor(.mGray1, for: .normal)
+        $0.titleLabel?.font = UIFont(name: "Noto Sans-Medium", size: 14)
+        $0.backgroundColor = .mGray5
+        $0.layer.cornerRadius = 10
+        $0.configuration = .plain()
+        $0.configuration?.image = UIImage(named: "mumentSearch")
+//        $0.configuration?.contentInsets = .init(top:0,leading: self.frame.width-140,bottom:0,trailing:0)
+        $0.configuration?.imagePadding = 10
+//        $0.titleLabel?.textAlignment = .left
+        $0.contentHorizontalAlignment = .left
+//        $0.configuration?.cornerStyle = 10
+    }
+    
     // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -36,7 +54,7 @@ class TVHeader: UIView {
 extension TVHeader {
     
     private func setLayout() {
-        self.addSubviews([logoButton,notificationButton])
+        self.addSubviews([logoButton,notificationButton,searchButton])
         
         logoButton.snp.makeConstraints{
             $0.leading.equalTo(self.safeAreaLayoutGuide).offset(20)
@@ -49,6 +67,13 @@ extension TVHeader {
             $0.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
             $0.top.equalTo(self.safeAreaLayoutGuide).offset(11)
             $0.width.height.equalTo(23.adjustedH)
+        }
+        
+        searchButton.snp.makeConstraints{
+            $0.leading.equalTo(self.safeAreaLayoutGuide).offset(20)
+            $0.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+            $0.top.equalTo(logoButton.snp.bottom).offset(15)
+            $0.height.equalTo(40.adjustedH)
         }
     }
 }
