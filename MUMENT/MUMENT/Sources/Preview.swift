c8d9d13360ac0e0c4408fd454e2db251b14c8282
Jimin Kim <0305jimin@gmail.com>
2022-07-14 03:06:58 +0900

    Chore: UIFont+ 미사용 코드 수정#14

--- MUMENT/MUMENT/Sources/Preview.swift
+++ MUMENT/MUMENT/Sources/Preview.swift
@@ -13,6 +13,8 @@ import SwiftUI
 struct ViewController_Preview: PreviewProvider {
     static var previews: some View {
         HomeVC().showPreview(.iPhone13mini)
+        WriteVC().showPreview(.iPhone13mini)
+        StorageVC().showPreview(.iPhoneSE2)
         HomeVC().showPreview(.iPhoneSE2)
         HomeVC().showPreview(.iPhone8)
         HomeVC().showPreview(.iPhone13Pro)
