9506b3bc5817114fa8ad22181fdf1c3d689fefa1
Jimin Kim <0305jimin@gmail.com>
2022-07-13 17:08:17 +0900

    Refactor: 코드리뷰 반영
    컴포넌트 클래스 이름 변경, 모델 프로퍼티 이름 변경

--- MUMENT/MUMENT/Sources/Scenes/Home/MumentForTodayTVC.swift
+++ MUMENT/MUMENT/Sources/Scenes/Home/MumentForTodayTVC.swift
@@ -12,13 +12,13 @@ import Then
 class MumentForTodayTVC: UITableViewCell {
     
     // MARK: - Properties
-    var dataSource: [MumentWithHeartModel] = MumentWithHeartModel.sampleData
+    var dataSource: [DefaultMumentModel] = DefaultMumentModel.sampleData
     lazy var titleLabel = UILabel().then{
         $0.text = "오늘의 뮤멘트"
         $0.textColor = .mBlack1
         $0.font = .mumentH2B18
     }
-    private let mumentCardView = MumentWithHeartView()
+    private let mumentCardView = DefaultMumentView()
     
     // MARK: - Initialization
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
