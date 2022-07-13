4027112ce112e22a054b094ebdeec9879c44f4a0
Jimin Kim <0305jimin@gmail.com>
2022-07-14 02:41:30 +0900

    Chore: Preview 파일 생성

---
+++ MUMENT/MUMENT/Sources/Preview.swift
@@ -0,0 +1,21 @@
+//
+//  Preview.swift
+//  MUMENT
+//
+//  Created by 김지민 on 2022/07/14.
+//
+
+
+// MARK: - SwiftUI Preview
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        HomeVC().showPreview(.iPhone13mini)
+        HomeVC().showPreview(.iPhoneSE2)
+        HomeVC().showPreview(.iPhone8)
+        HomeVC().showPreview(.iPhone13Pro)
+    }
+}
+#endif
