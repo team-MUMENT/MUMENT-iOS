b255f3c7db74c62547b765bbe56183c240f8c939
Jimin Kim <0305jimin@gmail.com>
2022-07-15 04:55:24 +0900

    Feat: DefaultNavigationBar 생성

--- MUMENT/MUMENT/Sources/Scenes/Home/DefaultNavigationBar.swift
+++ MUMENT/MUMENT/Sources/Scenes/Home/DefaultNavigationBar.swift
@@ -9,37 +9,50 @@ import UIKit
 import SnapKit
 import Then
 
-class DefaultSongDetailHeader: UIView {
+class DefaultNavigationBar: UIView {
     
     // MARK: - Properties
     private let backbutton = UIButton().then{
         $0.setImage(UIImage(named: "backIcon"), for: .normal)
         $0.configuration = .plain()
     }
+    
+    private let titleLabel = UILabel().then{
+        $0.textColor = .mBlack1
+        $0.font = .mumentH2B18
+    }
 
     // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: .zero)
-        setLayout()
+        setDefaultLayout()
     }
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
-        setLayout()
+        setDefaultLayout()
+    }
+    
+    //MARK: - Functions
+    func setTitle(_ title:String){
+        titleLabel.text = title
     }
 }
 
 // MARK: - UI
-extension DefaultSongDetailHeader {
+extension DefaultNavigationBar {
     
-    private func setLayout() {
-        self.addSubviews([backbutton])
+    private func setDefaultLayout() {
+        self.addSubviews([backbutton,titleLabel])
         
         backbutton.snp.makeConstraints{
             $0.leading.equalTo(self.safeAreaLayoutGuide).offset(5)
-            $0.top.equalTo(self.safeAreaLayoutGuide).offset(5)
+            $0.top.equalTo(self.safeAreaLayoutGuide)
             $0.height.width.equalTo(48)
-            
+        }
+        
+        titleLabel.snp.makeConstraints{
+            $0.centerX.centerY.equalTo(self.safeAreaLayoutGuide)
         }
     }
 }
---
+++ MUMENT/MUMENT/Sources/Scenes/Home/Model/DefaultNavigationBarModel.swift
@@ -0,0 +1,20 @@
+//
+//  DefaultHomeNavigationBarModel.swift
+//  MUMENT
+//
+//  Created by 김지민 on 2022/07/15.
+//
+
+import UIKit
+
+struct DefaultNavigationBarModel {
+    let title: String
+}
+
+//// MARK: - Extensions
+//extension DefaultNavigationBarModel {
+//    static var sampleData: [DefaultNavigationBarModel] = [
+//        DefaultNavigationBarModel(title:"")
+//    ]
+//}
+
