94c1c89de2769004b1d126731e79ae1b4728fed1
dev-madilyn <lollin0@naver.com>
2022-07-14 20:51:44 +0900

    Chore: TextView 자동완성 허용 #43

--- MUMENT/MUMENT/Sources/Scenes/Write/WriteVC.swift
+++ MUMENT/MUMENT/Sources/Scenes/Write/WriteVC.swift
@@ -92,7 +92,6 @@ class WriteVC: BaseVC {
         $0.textContainerInset = UIEdgeInsets(top: 15, left: 13, bottom: 15, right: 13)
         $0.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 30, right: 0)
         $0.font = .mumentB6M13
-        $0.autocorrectionType = .no
         $0.autocapitalizationType = .none
         $0.textColor = .mBlack2
     }
