cc3ec93e6060c527d2b4da3f8f2e2d09aa254a7f
Jimin Kim <0305jimin@gmail.com>
2022-07-15 02:12:06 +0900

    Feat: 테이블 뷰 셀 구현
    태그 부분 제외

--- MUMENT/MUMENT/Sources/Scenes/Home/SongDetailVC.swift
+++ MUMENT/MUMENT/Sources/Scenes/Home/SongDetailVC.swift
@@ -31,7 +31,7 @@ class SongDetailVC: BaseVC {
         mumentTV.delegate = self
         mumentTV.dataSource = self
         
-        mumentTV.register(cell: SongDetailTVC.self, forCellReuseIdentifier: SongDetailTVC.className)
+        mumentTV.register(cell: MumentCardBySongTVC.self, forCellReuseIdentifier: MumentCardBySongTVC.className)
         
 //        mumentTV.estimatedRowHeight = 44
 //        mumentTV.rowHeight = 48
@@ -54,11 +54,14 @@ extension SongDetailVC {
         songInfoView.snp.makeConstraints {
             $0.top.equalTo(navigationBarView.snp.bottom)
             $0.left.right.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(300)
         }
         
         mumentTV.snp.makeConstraints{
             $0.top.equalTo(songInfoView.snp.bottom)
-            $0.left.right.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.left.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.right.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
 }
@@ -84,15 +87,17 @@ extension SongDetailVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.section {
         case 0:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: SongDetailTVC.className, for: indexPath) as? SongDetailTVC else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MumentCardBySongTVC.className, for: indexPath) as? MumentCardBySongTVC else {
                 return UITableViewCell()
             }
+            cell.setData(myMumentDataSource[0])
             return cell
             
         case 1:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: SongDetailTVC.className, for: indexPath) as? SongDetailTVC else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MumentCardBySongTVC.className, for: indexPath) as? MumentCardBySongTVC else {
                 return UITableViewCell()
             }
+            cell.setData(allMumentsDataSource[indexPath.row])
             return cell
             
         default:
