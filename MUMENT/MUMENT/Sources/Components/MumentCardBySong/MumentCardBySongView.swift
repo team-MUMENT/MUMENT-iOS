b868c4a70cf1c124fee3f9c89dfcfb20076a83c3
Jimin Kim <0305jimin@gmail.com>
2022-07-15 01:30:27 +0900

    Feat: 곡 상세보기 뷰 세팅

---
+++ MUMENT/MUMENT/Sources/Components/MumentCardBySong/MumentCardBySongView.swift
@@ -0,0 +1,140 @@
+//
+//  MumentCardBySongView.swift
+//  MUMENT
+//
+//  Created by 김지민 on 2022/07/15.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MumentCardBySongView: UIView {
+    
+    // MARK: - Properties
+    lazy var writerInfoStackView = UIStackView(arrangedSubviews: [profileImage, writerNameLabel]).then{
+        $0.axis = .horizontal
+        $0.spacing = 7
+    }
+    let profileImage = UIImageView().then{
+        $0.makeRounded(cornerRadius: 12.5)
+    }
+    let writerNameLabel = UILabel().then{
+        $0.textColor = .mBlack2
+        $0.font = .mumentC1R12
+        $0.sizeToFit()
+    }
+    
+    private let heartButton = UIButton().then{
+        var configuration = UIButton.Configuration.plain()
+            configuration.imagePadding = 5
+            configuration.buttonSize = .small
+        $0.configuration = configuration
+    }
+    
+    let attributes: [NSAttributedString.Key: Any] = [
+        .font: UIFont.mumentC1R12,
+        .foregroundColor: UIColor.mGray1
+    ]
+    
+    var heartButtonText: String = "" {
+        didSet{
+            heartButton.setAttributedTitle(NSAttributedString(string: heartButtonText,attributes: attributes), for: .normal)
+        }
+    }
+    
+    let separatorView = UIView().then{
+        $0.backgroundColor = .mGray4
+    }
+    
+    ///data에 있는 것 만큼 DefaultTagView()하고 stack view에 추가
+    let tagStackView = UIStackView()
+    let contentsLabel = UILabel().then{
+        $0.textColor = .mBlack2
+        $0.lineBreakMode = .byCharWrapping
+        $0.numberOfLines = 2
+        $0.font = UIFont(name: "NotoSans-Medium", size: 13.0)
+    }
+    let createdAtLabel = UILabel().then{
+        $0.textColor = .mGray2
+        $0.font = .mumentC1R12
+    }
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setDefaultUI()
+        setDefaultLayout()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        setDefaultUI()
+        setDefaultLayout()
+    }
+    
+    //MARK: - Functions
+    func setData(_ cellData: MumentCardBySongModel){
+        profileImage.image = cellData.profileImage
+        writerNameLabel.text = cellData.writerName
+        contentsLabel.text = cellData.contentsLabel
+        createdAtLabel.text = cellData.createdAtLabel
+        heartButton.setImage(cellData.heartImage, for: .normal)
+        heartButtonText = "\(cellData.heartCount)"
+    }
+}
+
+// MARK: - UI
+extension MumentCardBySongView {
+    
+    func setDefaultUI(){
+        self.backgroundColor = .mWhite
+        self.makeRounded(cornerRadius: 11)
+        self.addShadow(offset: CGSize(width: 0, height: -2),opacity: 0.2,radius: 8.0)
+    }
+    
+    func setDefaultLayout() {
+        self.addSubviews([writerInfoStackView,heartButton,separatorView,tagStackView,contentsLabel,createdAtLabel])
+
+        writerInfoStackView.snp.makeConstraints {
+            $0.left.equalTo(self.safeAreaLayoutGuide).offset(13)
+            $0.top.equalTo(self.safeAreaLayoutGuide).offset(11)
+        }
+        
+        heartButton.snp.makeConstraints {
+            $0.right.equalTo(self.safeAreaLayoutGuide).inset(5)
+            $0.top.equalTo(self.safeAreaLayoutGuide).offset(9)
+        }
+        
+        separatorView.snp.makeConstraints{
+            $0.left.equalTo(self.safeAreaLayoutGuide).offset(13)
+            $0.right.equalTo(self.safeAreaLayoutGuide).inset(13)
+            $0.top.equalTo(writerInfoStackView.snp.bottom).offset(11)
+            $0.height.equalTo(1)
+        }
+        
+        tagStackView.snp.makeConstraints{
+            $0.left.equalTo(self.safeAreaLayoutGuide).offset(13)
+            $0.right.equalTo(self.safeAreaLayoutGuide).inset(13)
+            $0.top.equalTo(separatorView.snp.bottom).offset(11)
+        }
+        
+        contentsLabel.snp.makeConstraints{
+            $0.left.equalTo(self.safeAreaLayoutGuide).offset(13)
+            $0.right.equalTo(self.safeAreaLayoutGuide).inset(13)
+            $0.top.equalTo(tagStackView.snp.bottom).offset(10)
+        }
+        
+        createdAtLabel.snp.makeConstraints{
+            $0.left.equalTo(self.safeAreaLayoutGuide).offset(13)
+            $0.top.equalTo(contentsLabel.snp.bottom).offset(12)
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).inset(16)
+        }
+        
+
+        profileImage.snp.makeConstraints{
+            $0.height.width.equalTo(25)
+        }
+    }
+}
+
---
+++ MUMENT/MUMENT/Sources/Scenes/Home/SongDetailVC.swift
@@ -0,0 +1,117 @@
+//
+//  SongDetailVC.swift
+//  MUMENT
+//
+//  Created by 김지민 on 2022/07/15.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class SongDetailVC: BaseVC {
+   
+    // MARK: - Properties
+    private let navigationBarView = UIView()
+    private let songInfoView = SongInfoView()
+    private let mumentTV = UITableView()
+    
+    var myMumentDataSource: [MumentCardBySongModel] = MumentCardBySongModel.myMumentSampleData
+    var allMumentsDataSource: [MumentCardBySongModel] = MumentCardBySongModel.allMumentsSampleData
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTV()
+        setLayout()
+    }
+    
+    // MARK: - Functions
+    private func setTV() {
+        mumentTV.delegate = self
+        mumentTV.dataSource = self
+        
+        mumentTV.register(cell: SongDetailTVC.self, forCellReuseIdentifier: SongDetailTVC.className)
+        
+//        mumentTV.estimatedRowHeight = 44
+//        mumentTV.rowHeight = 48
+        mumentTV.separatorStyle = .none
+        mumentTV.showsVerticalScrollIndicator = false
+    }
+}
+
+// MARK: - UI
+extension SongDetailVC {
+    
+    private func setLayout() {
+        view.addSubviews([navigationBarView,songInfoView,mumentTV])
+        
+        navigationBarView.snp.makeConstraints {
+            $0.top.left.right.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(48)
+        }
+        
+        songInfoView.snp.makeConstraints {
+            $0.top.equalTo(navigationBarView.snp.bottom)
+            $0.left.right.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        mumentTV.snp.makeConstraints{
+            $0.top.equalTo(songInfoView.snp.bottom)
+            $0.left.right.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension SongDetailVC: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0 :
+            return myMumentDataSource.count
+        case 1:
+            return allMumentsDataSource.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SongDetailTVC.className, for: indexPath) as? SongDetailTVC else {
+                return UITableViewCell()
+            }
+            return cell
+            
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SongDetailTVC.className, for: indexPath) as? SongDetailTVC else {
+                return UITableViewCell()
+            }
+            return cell
+            
+        default:
+            return UITableViewCell()
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension SongDetailVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var cellHeight: CGFloat
+        switch indexPath.section {
+        case 0,1:
+            cellHeight = 200
+        default:
+            cellHeight = 0
+        }
+        return cellHeight
+    }
+}
+
