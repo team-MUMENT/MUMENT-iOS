66afb15205cf64c62ddfdc80b0856500ddc46aa5
dev-madilyn <lollin0@naver.com>
2022-07-15 22:45:32 +0900

    Add: RecentSearchTVC

---
+++ MUMENT/MUMENT/Sources/Scenes/Search/RecentSearchTVC.swift
@@ -0,0 +1,81 @@
+//
+//  RecentSearchTVC.swift
+//  MUMENT
+//
+//  Created by madilyn on 2022/07/15.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class RecentSearchTVC: UITableViewCell {
+    
+    // MARK: - Properties
+    private let albumImageView = UIImageView()
+    private let titleLabel = UILabel().then {
+        $0.font = .mumentB4M14
+        $0.textColor = .mBlack1
+    }
+    private let artistLabel = UILabel().then {
+        $0.font = .mumentB4M14
+        $0.textColor = .mBlack1
+    }
+    let removeButton = UIButton(type: .system).then {
+        $0.setImage(UIImage(named: "mumentDelete"), for: .normal)
+    }
+    
+    // MARK: - Initialization
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setLayout()
+        setUI()
+        selectionStyle = .none
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setData(data: MusicForSearchModel) {
+        albumImageView.setImageColor(color: .mGray5)
+        albumImageView.setImageUrl(data.imageUrl)
+        titleLabel.text = data.title
+        artistLabel.text = data.artist
+    }
+}
+
+// MARK: - UI
+extension RecentSearchTVC {
+    private func setLayout() {
+        self.contentView.addSubviews([albumImageView, titleLabel, artistLabel, removeButton])
+        
+        albumImageView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(10)
+            $0.leading.equalToSuperview().inset(20)
+            $0.width.equalTo(albumImageView.snp.height)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(albumImageView.snp.top).inset(3)
+            $0.leading.equalTo(albumImageView.snp.trailing).offset(10)
+        }
+        
+        artistLabel.snp.makeConstraints {
+            $0.bottom.equalTo(albumImageView.snp.bottom).inset(3)
+            $0.leading.equalTo(titleLabel.snp.leading)
+        }
+        
+        removeButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(20)
+            $0.centerY.equalTo(albumImageView)
+            $0.width.height.equalTo(24)
+        }
+    }
+    
+    private func setUI() {
+        self.backgroundColor = .mBgwhite
+    }
+}
+
