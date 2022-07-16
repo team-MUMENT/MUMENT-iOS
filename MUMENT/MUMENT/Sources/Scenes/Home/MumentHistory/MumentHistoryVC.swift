309b050961b74934f49871edb500dbffbb9eedf1
Jimin Kim <0305jimin@gmail.com>
2022-07-16 16:55:49 +0900

    Feat: 테이블 뷰 및 헤더 코드 작성 세팅#24

---
+++ MUMENT/MUMENT/Sources/Scenes/Home/MumentHistory/MumentHistoryVC.swift
@@ -0,0 +1,153 @@
+//
+//  MumentHistoryVC.swift
+//  MUMENT
+//
+//  Created by 김지민 on 2022/07/16.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MumentHistoryVC: BaseVC {
+    
+    // MARK: - Properties
+    private let navigationBarView = DefaultNavigationBar()
+//    private let detailScrollView = UIScrollView()
+//    private let detailContentView = UIView().then {
+//        $0.backgroundColor = .mBgwhite
+//    }
+//    private let songInfoView = SongInfoView()
+//    private let mumentCardView = DetailMumentCardView()
+    
+//    private let historyButton = UIButton().then{
+//        $0.makeRounded(cornerRadius: 11)
+//        $0.backgroundColor = .mGray4
+//        $0.configuration = .plain()
+//        $0.configuration?.image = UIImage(named: "rightArrow")
+//        $0.layer.cornerRadius = 10
+//        $0.configuration?.imagePadding = 120
+//        $0.configuration?.imagePlacement = .trailing
+//    }
+//
+//    let attributes: [NSAttributedString.Key: Any] = [
+//        .font: UIFont.mumentC1R12,
+//        .foregroundColor: UIColor.mGray1
+//    ]
+//
+//    var historyButtonText: String = "" {
+//        didSet{
+//            historyButton.setAttributedTitle(NSAttributedString(string: historyButtonText,attributes: attributes), for: .normal)
+//        }
+//    }
+    
+    private let mumentTV = UITableView( frame: CGRect.zero, style: .grouped)
+    
+    var musicInfodataSource: [SongDetailInfoModel] = SongDetailInfoModel.sampleData
+    var mumentDataSource: [MumentCardBySongModel] = MumentCardBySongModel.allMumentsSampleData
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+        setData()
+        
+    }
+    
+
+    // MARK: - Functions
+    private func setTV() {
+        mumentTV.delegate = self
+        mumentTV.dataSource = self
+        mumentTV.backgroundColor = .mBgwhite
+        mumentTV.register(cell: MumentCardBySongTVC.self, forCellReuseIdentifier: MumentCardBySongTVC.className)
+        mumentTV.register(MumentHistoryTVHeader.self, forHeaderFooterViewReuseIdentifier: MumentHistoryTVHeader.className)
+        
+        mumentTV.separatorStyle = .none
+        mumentTV.showsVerticalScrollIndicator = false
+    }
+    
+    func setData(){
+        navigationBarView.setTitle("뮤멘트 히스토리")
+//        mumentCardView.setData(dataSource[0])
+//        historyButtonText = "\(dataSource[0].mumentCount)개의 뮤멘트가 있는 히스토리 보러가기"
+    }
+}
+
+// MARK: - UI
+extension MumentHistoryVC {
+    
+    private func setLayout() {
+        view.addSubviews([navigationBarView,mumentTV])
+//        detailScrollView.addSubviews([detailContentView])
+//        detailContentView.addSubviews([mumentCardView,historyButton])
+        
+        navigationBarView.snp.makeConstraints {
+            $0.top.left.right.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(48)
+        }
+        
+        mumentTV.snp.makeConstraints{
+            $0.top.equalTo(navigationBarView.snp.bottom).offset(30)
+            $0.left.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.right.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(40)
+            $0.bottom.equalToSuperview().inset(20)
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension MumentHistoryVC: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0 :
+            return mumentDataSource.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MumentCardBySongTVC.className, for: indexPath) as? MumentCardBySongTVC else {
+                return UITableViewCell()
+            }
+            cell.setData(mumentDataSource[indexPath.row])
+            return cell
+        
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+            guard let headerCell = tableView.dequeueReusableHeaderFooterView(withIdentifier: MumentHistoryTVHeader.className) as? MyMumentSectionHeader else { return nil }
+            return headerCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 100
+    }
+    
+}
+
+// MARK: - UITableViewDelegate
+extension MumentHistoryVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var cellHeight: CGFloat
+        switch indexPath.section {
+        case 0:
+            cellHeight = 200
+        default:
+            cellHeight = 0
+        }
+        return cellHeight
+    }
+}
