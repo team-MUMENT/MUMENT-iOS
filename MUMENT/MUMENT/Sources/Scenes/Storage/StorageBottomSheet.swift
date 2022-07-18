f47e63cd08cc5b5f29cd7dc7a1a1557b7fcfc5c9
daminoworld <ekadls757@gmail.com>
2022-07-18 23:40:19 +0900

    Feat: 바텀시트 컬렉션 뷰 연결 #62

--- MUMENT/MUMENT/Sources/Scenes/Storage/StorageBottomSheet.swift
+++ MUMENT/MUMENT/Sources/Scenes/Storage/StorageBottomSheet.swift
@@ -57,10 +57,23 @@ class StorageBottomSheet: UIViewController {
         $0.font = .mumentB1B15
         $0.textColor = .mBlack2
     }
+    
+    private let impressionTagCV = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.backgroundColor = .mBgwhite
+        $0.showsHorizontalScrollIndicator = false
+        $0.isScrollEnabled = false
+    }
+    
     private let feelLabel = UILabel().then {
         $0.text = "감정"
         $0.font = .mumentB1B15
         $0.textColor = .mBlack2
+    }
+
+    private let feelTagCV = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.backgroundColor = .mBgwhite
+        $0.showsHorizontalScrollIndicator = false
+        $0.isScrollEnabled = false
     }
     
     var clickedTag: [Int] = []
@@ -68,69 +81,12 @@ class StorageBottomSheet: UIViewController {
     var impressionTagDummyData = ["🥁 비트", "🛫 도입부", "🎙 음색", "🎶 멜로디", "🎉 클라이막스", "💃 그루브"]
     var feelTagDummyData = ["🥁 비트", "🛫 도입부", "🎙 음색", "🎶 멜로디", "🎉 클라이막스", "💃 그루브", "🎡 벅참", "😄 신남", " 💐 설렘", "🗯 스트레스", "🗯 스트레스", "🗯 스트레스", "🗯 스트레스", "🗯 스트레스", "🗯 스트레스", "🗯 스트레스"]
     
-//    private let stackViewArray = [UIStackView]
-    
-    private let tagButton1 = UIButton().then {
-        var config = UIButton.Configuration.plain()
-        config.title = "🛫 도입부"
-        config.titleAlignment = .center
-        config.baseForegroundColor = UIColor.mGray1
-        config.baseBackgroundColor = UIColor.red
-        config.cornerStyle = .capsule
-        $0.configuration = config
-        
-        $0.makeRounded(cornerRadius: 19)
-        $0.titleLabel?.font = UIFont.mumentB4M14
-    }
-    
-    private let tagButton2 = UIButton().then {
-        $0.configuration = UIButton.Configuration.plain()
-        $0.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 8.0, leading: 13.0, bottom: 8.0, trailing: 13.0)
-        $0.backgroundColor = .mGray5
-        $0.makeRounded(cornerRadius: 19)
-        $0.titleLabel?.font = UIFont.mumentB4M14
-        $0.setTitle("🛫 도입부", for: .normal)
-        $0.setTitleColor(UIColor.mGray1, for: .normal)
-        $0.contentHorizontalAlignment = .center
-    }
-    
-    private let tagButton3 = UIButton().then {
-        $0.configuration = UIButton.Configuration.plain()
-        $0.backgroundColor = .mGray5
-        $0.makeRounded(cornerRadius: 19)
-        $0.titleLabel?.font = UIFont.mumentB4M14
-        $0.setTitle("🛫 도입부", for: .normal)
-        $0.setTitleColor(UIColor.mGray1, for: .normal)
-        $0.contentHorizontalAlignment = .center
-    }
-//    private let contentLabel = UILabel().then {
-//        $0.textAlignment = .center
-//        $0.font = .mumentB4M14
-//        $0.textColor = .mGray1
-//    }
-    
-    private let imPressionStackView = UIStackView().then {
-        $0.axis = .vertical
-        $0.distribution = .equalSpacing
-        $0.spacing = 10
-    }
-    
-    private let imPressionTagRowStackView = UIStackView().then {
-        $0.axis = .horizontal
-        $0.distribution = .equalSpacing
-        $0.spacing = 10
-    }
-    
-    private let feelStackView = UIStackView().then {
-        $0.axis = .vertical
-        $0.distribution = .equalSpacing
-        $0.spacing = 10
-    }
-    
-    private let feelTagRowStackView = UIStackView().then {
-        $0.axis = .horizontal
-        $0.distribution = .equalSpacing
-        $0.spacing = 10
+    private let tagCellHeight = 37
+    private let cellVerticalSpacing = 10
+    private let CVLayout = UICollectionViewFlowLayout().then {
+        $0.minimumLineSpacing = 10
+        $0.minimumInteritemSpacing = 10
+        $0.sectionInset = .zero
     }
     
     // MARK: - View Life Cycle
@@ -140,23 +96,37 @@ class StorageBottomSheet: UIViewController {
         setDismissButtonAction()
         tagCount = "1"
         
-        setStackViewLayout()
+        setTagCV()
+        registerCell()
+        setCVLayout()
     }
-    
-    /// 바텀시트 밖 터치했을때 dismiss (필요시 사용)
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        super.touchesBegan(touches, with: event)
-//        if let touch = touches.first,
-//           touch.view == self.view {
-//            hideBottomSheetWithAnimation()
-//        }
-//    }
 
     private func setDismissButtonAction() {
         dismissButton.press {
             self.isDismissed = true
             self.hideBottomSheetWithAnimation()
         }
+    }
+    
+    private func setTagCV() {
+        impressionTagCV.dataSource = self
+        impressionTagCV.delegate = self
+        impressionTagCV.layoutMargins = .zero
+        impressionTagCV.allowsMultipleSelection = true
+        impressionTagCV.clipsToBounds = true
+        impressionTagCV.collectionViewLayout = CVLayout
+        
+        feelTagCV.dataSource = self
+        feelTagCV.delegate = self
+        feelTagCV.layoutMargins = .zero
+        feelTagCV.allowsMultipleSelection = true
+        feelTagCV.clipsToBounds = true
+        feelTagCV.collectionViewLayout = CVLayout
+    }
+    
+    private func registerCell() {
+        impressionTagCV.register(cell: WriteTagCVC.self, forCellWithReuseIdentifier: WriteTagCVC.className)
+        feelTagCV.register(cell: WriteTagCVC.self, forCellWithReuseIdentifier: WriteTagCVC.className)
     }
 }
 
@@ -230,29 +200,96 @@ extension StorageBottomSheet {
     }
 }
 
+// MARK: - UICollectionViewDataSource
+extension StorageBottomSheet: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch collectionView {
+        case impressionTagCV:
+            return impressionTagDummyData.count
+        case feelTagCV:
+            return feelTagDummyData.count
+        default: return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WriteTagCVC.className, for: indexPath) as! WriteTagCVC
+        switch collectionView {
+        case impressionTagCV:
+            cell.setData(data: impressionTagDummyData[indexPath.row])
+            return cell
+        case feelTagCV:
+            cell.setData(data: feelTagDummyData[indexPath.row])
+            return cell
+        default: return cell
+        }
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension StorageBottomSheet: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sizingCell = WriteTagCVC()
+        switch collectionView {
+        case impressionTagCV:
+            sizingCell.setData(data: impressionTagDummyData[indexPath.row])
+        case feelTagCV:
+            sizingCell.setData(data: feelTagDummyData[indexPath.row])
+        default: break
+        }
+        
+        sizingCell.contentLabel.sizeToFit()
+        
+        let cellWidth = sizingCell.contentLabel.frame.width + 26
+        let cellHeight = tagCellHeight
+        return CGSize(width: cellWidth, height: CGFloat(cellHeight))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) as? WriteTagCVC {
+            cell.isSelected = true
+        }
+        debugPrint("cell clicked", "\(indexPath)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        
+        if let cell = collectionView.cellForItem(at: indexPath) as? WriteTagCVC {
+            cell.isSelected = false
+        }
+        debugPrint("cell Unclicked", "\(indexPath)")
+    }
+}
+
 // MARK: - BottomSheetTagView
 extension StorageBottomSheet {
-    func setStackViewLayout() {
-        containerView.addSubviews([impressionLabel, imPressionStackView])
-        
-        [imPressionTagRowStackView, imPressionTagRowStackView, imPressionTagRowStackView].forEach {
-            self.imPressionStackView.addArrangedSubview($0)
-        }
-        
-        [tagButton1, tagButton2, tagButton3].forEach {
-            self.imPressionTagRowStackView.addArrangedSubview($0)
-        }
-        
+    func setCVLayout() {
+        containerView.addSubviews([impressionLabel, impressionTagCV, feelLabel, feelTagCV])
+    
         impressionLabel.snp.makeConstraints {
             $0.top.equalTo(underLineView.snp.bottom).offset(45)
             $0.left.equalToSuperview().inset(22)
             $0.height.equalTo(16)
         }
         
-        imPressionStackView.snp.makeConstraints {
+        impressionTagCV.snp.makeConstraints {
             $0.top.equalTo(impressionLabel.snp.bottom).offset(16)
             $0.left.equalToSuperview().inset(20)
-            $0.height.equalTo(37.adjustedH)
+            $0.right.equalToSuperview().inset(86)
+            $0.height.equalTo(tagCellHeight * 2 + cellVerticalSpacing)
         }
+        
+        feelLabel.snp.makeConstraints {
+            $0.top.equalTo(impressionTagCV.snp.bottom).offset(33)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+        
+        feelTagCV.snp.makeConstraints {
+            $0.top.equalTo(feelLabel.snp.bottom).offset(16)
+            $0.left.equalToSuperview().inset(20)
+            $0.right.equalToSuperview().inset(19)
+            $0.height.equalTo(tagCellHeight * 5 + cellVerticalSpacing * 4)
+        }
+        
     }
 }
