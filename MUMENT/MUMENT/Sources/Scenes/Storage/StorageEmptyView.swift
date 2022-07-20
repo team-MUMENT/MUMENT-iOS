bbf3f76475abce3b0e797477cd79508bc1d8dd61
dev-madilyn <lollin0@naver.com>
2022-07-20 13:07:50 +0900

    Add: StorageEmptyView 추가 #76

---
+++ MUMENT/MUMENT/Sources/Scenes/Storage/StorageEmptyView.swift
@@ -0,0 +1,96 @@
+//
+//  StorageEmptyView.swift
+//  MUMENT
+//
+//  Created by madilyn on 2022/07/20.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class StorageEmptyView: UIView {
+    
+    // MARK: - Properties
+    private let imageView = UIImageView().then {
+        $0.image = UIImage(named: "likedMumentEmptyImage")
+    }
+    private let titleLabel = UILabel().then {
+        $0.text = """
+아직 기록한 뮤멘트가 없어요.
+"""
+        $0.font = .mumentH3B16
+        $0.textColor = .mGray1
+        $0.textAlignment = .center
+        $0.numberOfLines = 2
+    }
+    private let subTitleLabel = UILabel().then {
+        $0.text = "뮤멘트를 남겨 나만의 감상을 언제든 꺼내보세요."
+        $0.font = .mumentB6M13
+        $0.textColor = .mGray2
+        $0.textAlignment = .center
+    }
+    let writeButton = UIButton().then {
+        $0.setImage(UIImage(named: "recordBtn2"), for: .normal)
+    }
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setUI()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        setUI()
+    }
+}
+
+extension StorageEmptyView {
+    func setUI() {
+        self.backgroundColor = .mBgwhite
+    }
+    
+    func setMyMumentLayout() {
+        self.addSubviews([titleLabel, subTitleLabel, writeButton])
+        
+        titleLabel.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.top.equalToSuperview().inset(170.adjustedH)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        writeButton.snp.makeConstraints {
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(30.adjustedH)
+            $0.left.right.equalToSuperview().inset(88)
+            $0.height.equalTo(40)
+        }
+    }
+    
+    func setLikedMumentLayout() {
+        self.addSubviews([imageView, titleLabel, subTitleLabel])
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(130.adjustedH)
+            $0.width.equalTo(135.adjustedH)
+            $0.height.equalTo(109.adjustedH)
+            $0.centerX.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(20)
+            $0.centerX.equalTo(imageView)
+            $0.height.equalTo(22)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+            $0.centerX.equalTo(titleLabel)
+            $0.height.equalTo(20)
+        }
+    }
+}
