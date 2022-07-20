c1af2c323546e49417f12c39c0db86ad21f7e650
daminoworld <ekadls757@gmail.com>
2022-07-20 18:50:47 +0900

    Feat: bottom 부분 선택된 태그 영역 뷰 높이 동적 조절 기능 구현

--- MUMENT/MUMENT/Sources/Scenes/Storage/Model/TagButton.swift
+++ MUMENT/MUMENT/Sources/Scenes/Storage/Model/TagButton.swift
@@ -11,19 +11,25 @@ class TagButton: UIButton {
     override init(frame: CGRect){
        super.init(frame: frame)
    }
-   
+        
     init() {
         super.init(frame:CGRect.zero)
         self.backgroundColor = .mBlue3
         self.makeRounded(cornerRadius: 17)
-        self.titleLabel?.font = UIFont.mumentB2B14
-        self.setTitleColor(UIColor.mBlue1, for: .normal)
         self.setImage(UIImage(named: "mumentTagDelete"), for: .normal)
         self.contentHorizontalAlignment = .center
-//        self.semanticContentAttribute = .forceRightToLeft
         self.configuration = .plain()
         self.configuration?.imagePadding = 10.adjustedH
         self.configuration?.imagePlacement = .trailing
+        self.attributedTitle(for: .normal)
+    }
+    
+    func setTagButtonTitle(_ title: String) {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.mumentB2B14,
+            .foregroundColor: UIColor.mBlue1
+        ]
+        self.setAttributedTitle(NSAttributedString(string: title, attributes: attributes), for: .normal)
     }
     
     required init?(coder: NSCoder) {
