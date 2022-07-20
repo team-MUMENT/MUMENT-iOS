4f3f21e7649c886e657c9e542cc149a1c8f43150
dev-madilyn <lollin0@naver.com>
2022-07-21 01:37:42 +0900

    Chore: 네트워크 연결 실패 메시지 수정 #80

--- MUMENT/MUMENT/Sources/Scenes/Write/WriteVC.swift
+++ MUMENT/MUMENT/Sources/Scenes/Write/WriteVC.swift
@@ -325,7 +325,10 @@ extension WriteVC {
                     self.setRadioButtonSelectStatus(button: self.alreadyKnowButton, isSelected: !(result.isFirst))
                 }
             default:
-                self.makeAlert(title: "네트워크 안됨 지송")
+                self.makeAlert(title: """
+네트워크 오류로 인해 연결에 실패했어요! 🥲
+잠시 후에 다시 시도해 주세요.
+""")
             }
         }
     }
