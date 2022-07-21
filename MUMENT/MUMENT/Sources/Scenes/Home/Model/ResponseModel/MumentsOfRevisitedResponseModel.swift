5c415c63387fbaf9390f4e7d55066276dc6825ab
Jimin Kim <0305jimin@gmail.com>
2022-07-22 03:08:37 +0900

    Feat: 다시 들은 곡 섹션 서버 통신#82

--- MUMENT/MUMENT/Sources/Scenes/Home/Model/ResponseModel/MumentsOfRevisitedResponseModel.swift
+++ MUMENT/MUMENT/Sources/Scenes/Home/Model/ResponseModel/MumentsOfRevisitedResponseModel.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+
+// MARK: - MumentsOfRevisitedResponseModel
+struct MumentsOfRevisitedResponseModel: Codable {
+    let todayDate: String
+    let againMument: [AgainMument]
+
+    enum CodingKeys: String, CodingKey {
+        case todayDate = "todayDate"
+        case againMument = "againMument"
+    }
+    
+    // MARK: - AgainMument
+    struct AgainMument: Codable {
+        let music: Music
+        let user: User
+        let id: String
+        let mumentID: String
+        let isFirst: Bool?
+        let content: String
+        let createdAt: String
+        let displayDate: String
+
+        enum CodingKeys: String, CodingKey {
+            case music = "music"
+            case user = "user"
+            case id = "_id"
+            case mumentID = "mumentId"
+            case isFirst = "isFirst"
+            case content = "content"
+            case createdAt = "createdAt"
+            case displayDate = "displayDate"
+        }
+        
+        // MARK: - Music
+        struct Music: Codable {
+            let id: String
+            let name: String
+            let artist: String
+            let image: String
+
+            enum CodingKeys: String, CodingKey {
+                case id = "_id"
+                case name = "name"
+                case artist = "artist"
+                case image = "image"
+            }
+        }
+
+        // MARK: - User
+        struct User: Codable {
+            let id: String
+            let name: String
+            let image: String
+
+            enum CodingKeys: String, CodingKey {
+                case id = "_id"
+                case name = "name"
+                case image = "image"
+            }
+        }
+    }
+}
+
