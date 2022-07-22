9be5caa338bfc26dd6291f8b5615b969da537e5f
Jimin Kim <0305jimin@gmail.com>
2022-07-22 23:45:24 +0900

    //WIP
    라벨, 화살표 간격 문제 미해결

--- MUMENT/MUMENT/Sources/Scenes/Home/MumentDetail/DetailSongInfoView.swift
+++ MUMENT/MUMENT/Sources/Scenes/Home/MumentDetail/DetailSongInfoView.swift
@@ -17,12 +17,12 @@ class DetailSongInfoView: UIView {
         $0.clipsToBounds = true
     }
     
-    lazy var titleStackView = UIStackView(arrangedSubviews: [titleLabel, rightArrowImage]).then{
-        $0.axis = .horizontal
-        $0.spacing = 6
-        $0.distribution = .fillProportionally
-        $0.alignment = .center
-    }
+//    lazy var titleStackView = UIStackView(arrangedSubviews: [titleLabel, rightArrowImage]).then{
+//        $0.axis = .horizontal
+//        $0.spacing = 6
+//        $0.distribution = .fillProportionally
+//        $0.alignment = .center
+//    }
     private let titleLabel = UILabel().then{
         $0.textColor = .mBlack1
         $0.font = .mumentB2B14
@@ -52,17 +52,22 @@ class DetailSongInfoView: UIView {
         albumImage.image = cellData.albumImage
         titleLabel.text = cellData.songtitle
         artistLabel.text = cellData.artist
+        titleLabel.sizeToFit()
     }
     
-    func setData(albumURL: String,songTitle: String, artist: String){
+    func setData(albumURL: String, songTitle: String, artist: String){
         albumImage.setImageUrl(albumURL)
         titleLabel.text = songTitle
         artistLabel.text = artist
+        titleLabel.sizeToFit()
+//        titleLabel.adjustsFontSizeToFitWidth = true
+//        titleLabel.
     }
     
     func setData(_ cellData: HistoryResponseModel.DataMusic){
         albumImage.setImageUrl(cellData.image)
         titleLabel.text = cellData.name
+        titleLabel.sizeToFit()
         artistLabel.text = cellData.artist
     }
 }
@@ -71,7 +76,9 @@ class DetailSongInfoView: UIView {
 extension DetailSongInfoView {
     
     private func setLayout() {
-        self.addSubviews([albumImage,titleStackView,artistLabel])
+        self.addSubviews([albumImage,
+//                          titleStackView,
+                          titleLabel, rightArrowImage, artistLabel])
         
         albumImage.snp.makeConstraints{
             $0.top.equalTo(self.safeAreaLayoutGuide).offset(11)
@@ -79,20 +86,34 @@ extension DetailSongInfoView {
             $0.height.width.equalTo(55)
         }
         
-        titleStackView.snp.makeConstraints{
+//        titleStackView.snp.makeConstraints{
+//            $0.leading.equalTo(albumImage.snp.trailing).offset(10)
+//            $0.top.equalTo(self.safeAreaLayoutGuide).offset(19)
+//            $0.trailing.equalTo(self.safeAreaLayoutGuide)
+//        }
+//        titleLabel.setContentCompressionResistanceePriority(.init(rawValue: 998), for: .horizontal)
+        titleLabel.snp.makeConstraints{
             $0.leading.equalTo(albumImage.snp.trailing).offset(10)
-            $0.top.equalTo(self.safeAreaLayoutGuide).offset(19)
-            $0.trailing.equalTo(self.safeAreaLayoutGuide)
+            $0.top.equalToSuperview().offset(19)
+            
+//            $0.trailing.greaterThanOrEqualToSuperview().inset(6).priority(888)
+            
+        }
+//        rightArrowImage.setContentCompressionResistancePriority(.init(rawValue: 999), for: .horizontal)
+        rightArrowImage.snp.makeConstraints{
+            
+            $0.leading.equalTo(titleLabel.snp.trailing).offset(6)
+            $0.trailing.greaterThanOrEqualToSuperview().priority(300)
+            
+            $0.top.equalToSuperview().offset(25)
+//            $0.trailing.equalToSuperview()
+            $0.height.equalTo(10)
+            $0.width.equalTo(6)
         }
         
         artistLabel.snp.makeConstraints{
-            $0.top.equalTo(titleStackView.snp.bottom).offset(3)
+            $0.top.equalTo(rightArrowImage.snp.bottom).offset(3)
             $0.leading.equalTo(albumImage.snp.trailing).offset(10)
-        }
-        
-        rightArrowImage.snp.makeConstraints{
-            $0.height.equalTo(10)
-            $0.width.equalTo(6)
         }
     }
 }
