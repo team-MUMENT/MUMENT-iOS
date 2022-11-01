9dfde846ce03a35fbecae775a7af617eb0637be8
Jimin Kim <0305jimin@gmail.com>
2022-11-01 15:50:17 +0900

    Feat: 이미지 설정 및 레이아웃#142

--- MUMENT/MUMENT/Sources/Scenes/SignIn/SignInVC.swift
+++ MUMENT/MUMENT/Sources/Scenes/SignIn/SignInVC.swift
@@ -9,43 +9,88 @@ import UIKit
 import Then
 import SnapKit
 
-class SignInVC: BaseVC {
+final class SignInVC: BaseVC {
+    
+    // MARK: - Properties
+    private let logoImageView = UIImageView().then{
+        $0.image = UIImage(named: "mumentLogoLogin")
+    }
+    private let guidingLabel = UILabel().then{
+        $0.font = .mumentB4M14
+        $0.textColor = .mBlack2
+        $0.text = "회원가입을 통해 나의 음악 감상을 쌓아보세요."
+    }
+    private let kakaoSignInButton = UIButton().then{
+        $0.setBackgroundImage(UIImage(named: "kakaoLogin"), for: .normal)
+    }
+    private let appleSignInButton = UIButton().then{
+        $0.setBackgroundImage(UIImage(named: "appleLogin"), for: .normal)
+    }
+    // TODO: - NSAttributedString 이용해서 스타일 바꾸고 링크 연결하기
+    private let privacyPolicyLabel = UILabel().then{
+        $0.font = .mumentB8M12
+        $0.textColor = .mGray2
+        $0.text = "로그인 시 이용약관과\n개인정보처리방침에 동의하게 됩니다."
+        $0.numberOfLines = 2
+        $0.textAlignment = .center
+        $0.sizeToFit()
+    }
+    
+    // MARK: - Initialization
     override func viewDidLoad() {
         super.viewDidLoad()
-        requestSignIn(body: SignInBodyModel(profileId: "mumentdsf", password: "mumentasdlf"))
-       // 이렇게 쓰세요.
-//        completeBtn.press { [weak self] in
-//            self?.requestSignIn(body: self?.signData ?? SignBodyModel(name: "", email: "", password: ""), completion: { userName in
-//                self?.makeAlert(title: "로그인 성공", message: "", okTitle: "확인", okAction: { alert in
-//                    let tabBar = InstagramTBC()
-//                    tabBar.modalPresentationStyle = .fullScreen
-//                    self?.present(tabBar, animated: true)
-//                })
-//            })
-//        }
+        setBackgroundImage()
+        setLayout()
+        setButtonActions()
+    }
+    
+    // MARK: - Functions
+    func setButtonActions(){
+        kakaoSignInButton.press{
+            print("카카오로 계속하기 버튼 클릭됨")
+        }
+        
+        appleSignInButton.press{
+            print("Apple로 계속하기 버튼 클릭됨")
+        }
     }
 }
 
-// MARK: - Network
+// MARK: - UI
 extension SignInVC {
-    private func requestSignIn(body: SignInBodyModel) {
-        SignAPI.shared.postSignIn(body: body) { networkResult in
-            switch networkResult {
-            case .success(let response):
-                if response is SignInBodyModel {
-                    print(response)
-                }
-//            case .requestErr(_):
-//                // 에러 처리...
-//            case .pathErr:
-//                // 에러 처리...
-//            case .serverErr:
-//                // 에러 처리...
-//            case .networkFail:
-//                // 에러 처리...
-            default:
-                self.makeAlert(title: MessageType.networkError.message)
-            }
+    private func setBackgroundImage(){
+        let backgroundImageView = UIImageView(frame: self.view.frame)
+        backgroundImageView.image = UIImage(named: "signInBackground")
+        backgroundImageView.contentMode = .scaleAspectFit
+        self.view.addSubview(backgroundImageView)
+    }
+    
+    private func setLayout() {
+        view.addSubviews([logoImageView,guidingLabel,kakaoSignInButton,appleSignInButton,privacyPolicyLabel])
+        
+        logoImageView.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(168)
+            $0.centerX.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        guidingLabel.snp.makeConstraints{
+            $0.top.equalTo(logoImageView.snp.bottom).offset(100)
+            $0.centerX.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        kakaoSignInButton.snp.makeConstraints{
+            $0.top.equalTo(guidingLabel.snp.bottom).offset(17)
+            $0.centerX.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        appleSignInButton.snp.makeConstraints{
+            $0.top.equalTo(kakaoSignInButton.snp.bottom).offset(15)
+            $0.centerX.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        privacyPolicyLabel.snp.makeConstraints{
+            $0.top.equalTo(appleSignInButton.snp.bottom).offset(40)
+            $0.centerX.equalTo(view.safeAreaLayoutGuide)
         }
     }
 }
