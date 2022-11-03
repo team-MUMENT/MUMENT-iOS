c5df5b7652d8bb5b5148afb84e6519dcbf4ebd5c
dev-madilyn <lollin0@naver.com>
2022-11-03 23:13:40 +0900

    ✅ Chore: 좌측 정렬 CollectionViewFlowLayout 수정 (#150)

--- MUMENT/MUMENT/Sources/Scenes/Write/LeftAlignedCollectionViewFlowLayout.swift
+++ MUMENT/MUMENT/Sources/Scenes/Write/LeftAlignedCollectionViewFlowLayout.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
     let cellSpacing: CGFloat = 10
- 
+    
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         self.minimumLineSpacing = 10.0
-        self.sectionInset = .zero
-        let attributes = super.layoutAttributesForElements(in: rect)
- 
-        var leftMargin = sectionInset.left
+        self.minimumInteritemSpacing = 10
+        self.sectionInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        
+        let attributes = super.layoutAttributesForElements(in: rect)?.map { $0.copy() as! UICollectionViewLayoutAttributes }
+        var leftMargin: CGFloat = sectionInset.left
         var maxY: CGFloat = -1.0
         attributes?.forEach { layoutAttribute in
+            guard layoutAttribute.representedElementCategory == .cell else { return }
             if layoutAttribute.frame.origin.y >= maxY {
-                leftMargin = sectionInset.left
+                leftMargin = 15.0
             }
             layoutAttribute.frame.origin.x = leftMargin
-            leftMargin += layoutAttribute.frame.width + cellSpacing
-            maxY = max(layoutAttribute.frame.maxY, maxY)
+            leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
+            maxY = max(layoutAttribute.frame.maxY , maxY)
         }
         return attributes
     }
