f1d66f559f1ec4a5b7729ba59d89ae0870e67609
dev-madilyn <lollin0@naver.com>
2022-11-15 02:12:52 +0900

    Feature: 프로필 수정 뷰 레이아웃 구현 #165

---
+++ MUMENT/MUMENT/Sources/Scenes/Mypage/SetProfile/SetProfileVC.swift
@@ -0,0 +1,81 @@
+//
+//  SetProfileVC.swift
+//  MUMENT
+//
+//  Created by madilyn on 2022/11/14.
+//
+
+import UIKit
+import SnapKit
+import Then
+import RxCocoa
+import RxSwift
+
+final class SetProfileVC: BaseVC {
+    
+    // MARK: Components
+    private let naviView: DefaultNavigationBar = DefaultNavigationBar(naviType: .leftArrowRightDone).then {
+        $0.setTitleLabel(title: "프로필 설정")
+        $0.doneButton.isEnabled = false
+    }
+    private let loadImageButton: UIButton = UIButton(type: .system).then {
+        $0.setImage(UIImage(named: "mumentProfileCamera"), for: .normal)
+    }
+    private let nickNameTextField: MumentTextField = MumentTextField().then {
+        $0.placeholder = "닉네임을 입력해주세요. (필수)"
+    }
+    private let infoLabel: UILabel = UILabel().then {
+        $0.text = "특수문자 제외 2-15자"
+        $0.font = .mumentB8M12
+        $0.textColor = .mGray2
+        $0.sizeToFit()
+    }
+    private let nickNameCountLabel: UILabel = UILabel().then {
+        $0.text = "0/15"
+        $0.textColor = .mGray1
+        $0.setColor(to: "0", with: .mPurple1)
+        $0.font = .mumentB8M12
+        $0.sizeToFit()
+    }
+    
+    // MARK: Properties
+    private let disposeBag: DisposeBag = DisposeBag()
+    
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setLayout()
+    }
+    
+}
+
+// MARK: - UI
+extension SetProfileVC {
+    private func setLayout() {
+        self.view.addSubviews([naviView, loadImageButton, nickNameTextField, infoLabel, nickNameCountLabel])
+        
+        naviView.snp.makeConstraints {
+            $0.top.left.right.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(48)
+        }
+        loadImageButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(131)
+            $0.top.equalTo(naviView.snp.bottom).offset(79)
+        }
+        nickNameTextField.snp.makeConstraints {
+            $0.top.equalTo(loadImageButton.snp.bottom).offset(64)
+            $0.left.right.equalToSuperview().inset(20)
+            $0.height.equalTo(50)
+        }
+        infoLabel.snp.makeConstraints {
+            $0.top.equalTo(nickNameTextField.snp.bottom).offset(14)
+            $0.left.equalTo(nickNameTextField)
+        }
+        nickNameCountLabel.snp.makeConstraints {
+            $0.centerY.equalTo(infoLabel)
+            $0.right.equalTo(nickNameTextField)
+        }
+    }
+}
