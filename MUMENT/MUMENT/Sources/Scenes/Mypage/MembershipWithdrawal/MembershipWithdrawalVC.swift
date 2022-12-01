555dad48631be216be0b1a2ea1506d1b4928b6d0
Jimin Kim <0305jimin@gmail.com>
2022-12-01 23:30:59 +0900

    Chore: 코드 정리 #146

--- MUMENT/MUMENT/Sources/Scenes/Mypage/MembershipWithdrawal/MembershipWithdrawalVC.swift
+++ MUMENT/MUMENT/Sources/Scenes/Mypage/MembershipWithdrawal/MembershipWithdrawalVC.swift
@@ -10,7 +10,7 @@ import SnapKit
 import Then
 
 final class MembershipWithdrawalVC: BaseVC {
-
+    
     // MARK: Properties
     private var userName: String = ""{
         didSet{
@@ -19,86 +19,86 @@ final class MembershipWithdrawalVC: BaseVC {
     }
     
     // MARK: - Components
-    private let naviView = DefaultNavigationBar(naviType: .leftArrow).then {
+    private let naviView: DefaultNavigationBar = DefaultNavigationBar(naviType: .leftArrow).then {
         $0.setTitleLabel(title: "회원탈퇴")
     }
     
-    private let imageView = UIImageView().then {
+    private let imageView: UIImageView = UIImageView().then {
         $0.image = UIImage(named: "mumentProfileSad601")
     }
     
-    private let headingLabel = UILabel().then {
+    private let headingLabel: UILabel = UILabel().then {
         $0.text = "정말 떠나시는 건가요?"
         $0.font = .mumentH2B18
         $0.textColor = .mBlack1
     }
     
-    private let noticeLabel = UILabel().then {
-        $0.text = "지금 당장 뮤멘트를 떠나시면 곡에 담긴 추억이 모두 사라지게 돼요. 00님이 좋아하신 뮤멘트들도 더이상 모아볼 수 없게 됩니다."
+    private let noticeLabel: UILabel = UILabel().then {
+        $0.text = "지금 당장 뮤멘트를 떠나시면 곡에 담긴 추억이 모두 사라지게 돼요. 사용자 님이 좋아하신 뮤멘트들도 더이상 모아볼 수 없게 됩니다."
         $0.font = .mumentB3M14
         $0.textColor = .mBlack2
         $0.numberOfLines = 3
         $0.lineBreakMode = .byCharWrapping
     }
     
-    private let inquiryLabel = UILabel().then {
+    private let inquiryLabel: UILabel = UILabel().then {
         $0.text = "탈퇴하시려는 이유가 궁금해요."
         $0.font = .mumentB4M14
         $0.textColor = .mBlack1
     }
-
+    
     private let reasonSelectionButton: DropDownButton = DropDownButton(title: "이유 선택")
     
-    private let checkBoxButton = UIButton().then {
+    private let checkBoxButton: UIButton = UIButton().then {
         $0.setBackgroundImage(UIImage(named: "mumentUnchecked"), for: .normal)
     }
     
-    private let reconfirmingLabel = UILabel().then {
+    private let reconfirmingLabel: UILabel = UILabel().then {
         $0.text = "안내사항을 모두 확인했으며, 탈퇴를 신청합니다."
         $0.font = .mumentB8M12
     }
     
-    lazy var reconfirmingStackView = UIStackView(arrangedSubviews: [checkBoxButton, reconfirmingLabel]).then{
+    lazy var reconfirmingStackView: UIStackView = UIStackView(arrangedSubviews: [checkBoxButton, reconfirmingLabel]).then{
         $0.axis = .horizontal
         $0.spacing = 5
     }
     
-    private let withdrawalButton = MumentCompleteButton(isEnabled: false).then {
+    private let withdrawalButton: MumentCompleteButton = MumentCompleteButton(isEnabled: false).then {
         $0.setTitle("탈퇴하기", for: .normal)
     }
     
     // MARK: - View Life Cycle
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            setLayout()
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+    }
 }
 
 // MARK: - UI
 extension MembershipWithdrawalVC {
-
+    
     private func setLayout() {
         view.addSubviews([naviView, imageView, headingLabel, noticeLabel, inquiryLabel, reasonSelectionButton, withdrawalButton, reconfirmingStackView])
-
+        
         naviView.snp.makeConstraints {
             $0.left.top.right.equalTo(view.safeAreaLayoutGuide)
         }
-
+        
         imageView.snp.makeConstraints {
             $0.top.equalTo(naviView.snp.bottom).offset(22)
             $0.centerX.equalToSuperview()
         }
-
+        
         headingLabel.snp.makeConstraints {
             $0.top.equalTo(imageView.snp.bottom).offset(25)
             $0.centerX.equalToSuperview()
         }
-
+        
         noticeLabel.snp.makeConstraints {
             $0.top.equalTo(headingLabel.snp.bottom).offset(10)
             $0.horizontalEdges.equalToSuperview().inset(20)
         }
-
+        
         inquiryLabel.snp.makeConstraints {
             $0.top.equalTo(noticeLabel.snp.bottom).offset(25)
             $0.leading.equalToSuperview().offset(20)
@@ -108,7 +108,7 @@ extension MembershipWithdrawalVC {
             $0.top.equalTo(inquiryLabel.snp.bottom).offset(13)
             $0.leading.equalToSuperview().offset(20)
         }
-
+        
         withdrawalButton.snp.makeConstraints {
             $0.bottom.equalToSuperview().inset(39)
             $0.horizontalEdges.equalToSuperview().inset(20)
--- MUMENT/MUMENT/Sources/Scenes/Mypage/MembershipWithdrawal/ReasonSelectionButton.swift
+++ MUMENT/MUMENT/Sources/Scenes/Mypage/MembershipWithdrawal/ReasonSelectionButton.swift
@@ -10,11 +10,11 @@ import SnapKit
 import Then
 
 final class DropDownButton: UIButton {
-
+    
+    // MARK: - Components
     private var buttonTitleLabel = UILabel().then{
         $0.textColor =  .mGray1
         $0.font = .mumentB3M14
-        
     }
     
     // MARK: - Initialization
@@ -27,33 +27,19 @@ final class DropDownButton: UIButton {
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
     }
-    
-    // MARK: - UI
-    private func setUI(title: String) {
-//        self.isEnabled = isEnabled
-//        self.makeRounded(cornerRadius: 11.adjustedH)
-//        self.setBackgroundColor(.mGray5, for: .normal)
-//        self.setTitleColor(.mGray1, for: .normal)
-//        self.titleLabel?.font = .
-
-//        self.setBackgroundImage(UIImage(named: "dropDownButton_selected"), for: .normal)
-//        var configuration = UIButton.Configuration.
-//        configuration.image = UIImag/e(named: "dropDownButton_unselected")
-//        configuration.titleAlignment = .leading
-//        self.configuration = configuration
-//        self.setTitleWithCustom(title, font: .mumentB3M14, color: .mGray1, for: .normal)
-//        self.layer.borderColor = UIColor.mGray3.cgColor
-        self.setBackgroundImage(UIImage(named: "dropDownButton_unselected"), for: .normal)
-        buttonTitleLabel.text = title
-    }
 }
 
 // MARK: - UI
 extension DropDownButton {
-
+    
+    private func setUI(title: String) {
+        self.setBackgroundImage(UIImage(named: "dropDownButton_unselected"), for: .normal)
+        buttonTitleLabel.text = title
+    }
+    
     private func setLayout() {
         self.addSubview(buttonTitleLabel)
-
+        
         buttonTitleLabel.snp.makeConstraints {
             $0.left.equalToSuperview().offset(13)
             $0.centerY.equalToSuperview()
