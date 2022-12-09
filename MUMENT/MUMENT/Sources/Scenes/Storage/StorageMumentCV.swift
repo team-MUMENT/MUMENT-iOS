5d083de780db368664e343674a88a78e0996f7eb
daminoworld <ekadls757@gmail.com>
2022-12-09 20:19:37 +0900

    Chore: Funcitionalize layout code #183

--- MUMENT/MUMENT/Sources/Scenes/Storage/StorageMumentCV.swift
+++ MUMENT/MUMENT/Sources/Scenes/Storage/StorageMumentCV.swift
@@ -12,17 +12,19 @@ final class StorageMumentCV: UICollectionView {
     
     override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         super.init(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setLayout() {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         self.backgroundColor = .clear
         self.contentInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
         self.showsVerticalScrollIndicator = false
         self.collectionViewLayout = layout
-    }
-    
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
