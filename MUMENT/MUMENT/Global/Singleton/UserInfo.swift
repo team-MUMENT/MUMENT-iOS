4b0f1d320ef2e8a97c3e3a07905d284dff2b0586
dev-madilyn <lollin0@naver.com>
2023-01-16 03:48:23 +0900

    Chore: 임시 accessToken, refreshToken 추가 #199

--- MUMENT/MUMENT/Global/Singleton/UserInfo.swift
+++ MUMENT/MUMENT/Global/Singleton/UserInfo.swift
@@ -13,4 +13,6 @@ class UserInfo {
     private init() { }
     
     var userId: String?
+    var accessToken: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzQsInByb2ZpbGVJZCI6bnVsbCwiaW1hZ2UiOm51bGwsImlhdCI6MTY3MzgwODMyNCwiZXhwIjoxNjc2NDAwMzI0LCJpc3MiOiJNdW1lbnQifQ.dywLT7ymxSQatKZY1jritgAB0w1V_qGi48ORFY_3TtM"
+    var refreshToken: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzQsInByb2ZpbGVJZCI6bnVsbCwiaW1hZ2UiOm51bGwsImlhdCI6MTY3MzgwODMyNCwibmJmIjoxNjc2NDAwMzI0LCJleHAiOjE2Nzg5OTIzMjQsImlzcyI6Ik11bWVudCJ9.xeVUW00d16Y-cw6l4yI6EjwzvWa8nhQw3mS1W1cLcJI"
 }
