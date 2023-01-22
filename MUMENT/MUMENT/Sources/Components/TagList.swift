eb879be0da32f56fdaa49448bf1613234bdece40
daminoworld <ekadls757@gmail.com>
2023-01-23 02:39:08 +0900

    Refactor: Storage Filter Layout #202

---
+++ MUMENT/MUMENT/Sources/Components/TagList.swift
@@ -0,0 +1,13 @@
+//
+//  TagsList.swift
+//  MUMENT
+//
+//  Created by 김담인 on 2023/01/21.
+//
+
+import Foundation
+
+struct TagList {
+    let impressionTags: [String] = ["🎙 음색", "🥁 비트", "🖋 가사", "🎶 멜로디", "🎸 베이스", "🛫 도입부"]
+    let feelTags: [String] = ["🎡 벅참", "😄 신남", "💐 설렘", "😚 행복", "👥 위로", "😔 우울", "🕰 그리움", "🛌 외로움", "☕️ 차분", "💭 회상", "🍀 여유로움", "🌅 낭만", "🙌 자신감", "⌛️ 아련함", "🍁 센치함", "🌋 스트레스"]
+}
