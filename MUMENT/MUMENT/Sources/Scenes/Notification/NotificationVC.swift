63efca3321e9cae473714c925a54ab997f902bc2
dev-madilyn <lollin0@naver.com>
2023-01-29 17:03:27 +0900

    Feat: 소식창 UI 구현 #206

--- MUMENT/MUMENT/Sources/Scenes/Notification/NotificationVC.swift
+++ MUMENT/MUMENT/Sources/Scenes/Notification/NotificationVC.swift
@@ -25,7 +25,7 @@ final class NotificationVC: BaseVC {
     }
     
     // MARK: Properties
-    private let notificationData: [
+    private var notificationList: GetNotificationListResponseModel = []
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
@@ -33,6 +33,13 @@ final class NotificationVC: BaseVC {
         
         self.setLayout()
         self.hideTabbar()
+        self.setNotificationTV()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.getNotificationList()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -45,17 +52,20 @@ final class NotificationVC: BaseVC {
     private func setNotificationTV() {
         self.notificationTV.delegate = self
         self.notificationTV.dataSource = self
+        self.notificationTV.register(cell: NotificationTVC.self)
     }
 }
 
 // MARK: - UITableViewDelegate
 extension NotificationVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return
+        return notificationList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NotificationTVC.className) as? NotificationTVC else { return UITableViewCell() }
+        cell.setData(data: self.notificationList[indexPath.row])
+        return cell
     }
 }
 
@@ -67,11 +77,33 @@ extension NotificationVC: UITableViewDelegate {
 // MARK: - UI
 extension NotificationVC {
     private func setLayout() {
-        view.addSubviews([naviView])
+        view.addSubviews([naviView, notificationTV])
         
-        self.naviView.snp.makeConstraints {
-            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-            $0.height.equalTo(48)
+        self.naviView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(48)
+        }
+        
+        self.notificationTV.snp.makeConstraints { make in
+            make.top.equalTo(self.naviView.snp.bottom).offset(13)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Network
+extension NotificationVC {
+    private func getNotificationList() {
+        NotificationAPI.shared.getNotificationList { networkResult in
+            switch networkResult {
+            case .success(let t):
+                if let result = t as? GetNotificationListResponseModel {
+                    self.notificationList = result
+                    self.notificationTV.reloadData()
+                }
+            default:
+                self.makeAlert(title: MessageType.networkError.message)
+            }
         }
     }
 }
