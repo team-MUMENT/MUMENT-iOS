412976f3947581e30e99b6a59b693a18fe23c249
dev-madilyn <lollin0@naver.com>
2023-01-30 01:25:32 +0900

    Chore: 새로운 알림 읽음 처리 Service, API 구현 #206

--- MUMENT/MUMENT/Network/APIManagers/NotificationAPI.swift
+++ MUMENT/MUMENT/Network/APIManagers/NotificationAPI.swift
@@ -28,9 +28,24 @@ class NotificationAPI: BaseAPI {
         }
     }
     
-    /// [GET] 소식창 리스트 조회
+    /// [PATCH] 알림 삭제
     func deleteNotifiction(id: Int, completion: @escaping (NetworkResult<Any>) -> (Void)) {
         AFmanager.request(NotificationService.deleteNotification(newsId: id)).responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let data = response.data else { return }
+                let networkResult = self.judgeStatus(by: statusCode, data, String.self)
+                completion(networkResult)
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+    
+    /// [PATCH] 새로운 알림 읽음 처리
+    func readNotification(idList: [Int], completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        AFmanager.request(NotificationService.readNotification(newsIdList: idList)).responseData { response in
             switch response.result {
             case .success:
                 guard let statusCode = response.response?.statusCode else { return }
