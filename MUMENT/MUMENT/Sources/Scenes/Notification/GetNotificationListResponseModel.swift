115a551005c7f8190a97e8efa1c91bf4560678a9
dev-madilyn <lollin0@naver.com>
2023-01-29 17:04:16 +0900

    Chore: GetNotificationListResponseModelElement 모델 수정 #206

--- MUMENT/MUMENT/Sources/Scenes/Notification/GetNotificationListResponseModel.swift
+++ MUMENT/MUMENT/Sources/Scenes/Notification/GetNotificationListResponseModel.swift
@@ -10,18 +10,18 @@ import Foundation
 struct GetNotificationListResponseModelElement: Codable {
     var id: Int
     var type: String
-    var userID: Int
+    var userID: Int?
     var isDeleted, isRead: Bool
     var createdAt: String
     var linkID: Int
-    var noticeTitle, likeProfileID, likeMusicTitle: String?
+    var noticePoint, noticeTitle, likeProfileID, likeMusicTitle: String?
 
     enum CodingKeys: String, CodingKey {
         case id, type
         case userID = "userId"
         case isDeleted, isRead, createdAt
         case linkID = "linkId"
-        case noticeTitle
+        case noticePoint, noticeTitle
         case likeProfileID = "likeProfileId"
         case likeMusicTitle
     }
