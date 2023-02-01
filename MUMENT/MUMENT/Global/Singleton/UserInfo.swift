cd146b04232e1902cab85dd74a2af4c0e9e63ce9
dev-madilyn <lollin0@naver.com>
2023-02-02 05:35:46 +0900

    Fix: 회원탈퇴 및 로그아웃 로직 수정 #234

--- MUMENT/MUMENT/Global/Singleton/UserInfo.swift
+++ MUMENT/MUMENT/Global/Singleton/UserInfo.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class UserInfo {
-    static let shared = UserInfo()
+    static var shared = UserInfo()
     
-    private init() { }
+    init() { }
     
     var userId: Int?
-    var accessToken: String? = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDAsInByb2ZpbGVJZCI6Iu2FjOyKpO2KuDIiLCJpbWFnZSI6bnVsbCwiaWF0IjoxNjc0OTMyODA1LCJleHAiOjE2Nzc1MjQ4MDUsImlzcyI6Ik11bWVudCJ9.xfWBuIEC8h75pwSWfFowPByIhwyeriJmyls8h-BmcEM"
-    var refreshToken: String? = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDAsInByb2ZpbGVJZCI6Iu2FjOyKpO2KuDIiLCJpbWFnZSI6bnVsbCwiaWF0IjoxNjc0OTMyODA1LCJleHAiOjE2ODAxMTY4MDUsImlzcyI6Ik11bWVudCJ9.qTSYY9E3B5nNpfAYI2BWvr_hM1MA99zIrLCEZohDVHc"
+    var accessToken: String? = ""
+    var refreshToken: String? = ""
     var nickname: String = ""
     var profileImageURL: String = "https://mument.s3.ap-northeast-2.amazonaws.com/user/emptyImage.jpg"
     lazy var profileImage: UIImage = UIImage().setImageUrl(profileImageURL)
