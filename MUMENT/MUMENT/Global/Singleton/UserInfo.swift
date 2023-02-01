84829caabc2d0ae56e9316afbf5ef7cb4ea353df
dev-madilyn <lollin0@naver.com>
2023-02-02 04:54:54 +0900

    Chore: 로그인/마이페이지 구분하여 프로필 설정 뷰 로직 변경 #234

--- MUMENT/MUMENT/Global/Singleton/UserInfo.swift
+++ MUMENT/MUMENT/Global/Singleton/UserInfo.swift
@@ -5,7 +5,7 @@
 //  Created by madilyn on 2022/07/20.
 //
 
-import Foundation
+import UIKit
 
 class UserInfo {
     static let shared = UserInfo()
@@ -17,4 +17,5 @@ class UserInfo {
     var refreshToken: String? = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDAsInByb2ZpbGVJZCI6Iu2FjOyKpO2KuDIiLCJpbWFnZSI6bnVsbCwiaWF0IjoxNjc0OTMyODA1LCJleHAiOjE2ODAxMTY4MDUsImlzcyI6Ik11bWVudCJ9.qTSYY9E3B5nNpfAYI2BWvr_hM1MA99zIrLCEZohDVHc"
     var nickname: String = ""
     var profileImageURL: String = "https://mument.s3.ap-northeast-2.amazonaws.com/user/emptyImage.jpg"
+    lazy var profileImage: UIImage = UIImage().setImageUrl(profileImageURL)
 }
