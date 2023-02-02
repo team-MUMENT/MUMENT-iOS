740bc97ddc18f340945582bd3628318f539fc0d2
daminoworld <ekadls757@gmail.com>
2023-02-02 16:07:02 +0900

    Chore: Update LikeCancel Response Model #240

--- MUMENT/MUMENT/Sources/Scenes/Home/Model/ResponseModel/LikeCancelResponseModel.swift
+++ MUMENT/MUMENT/Sources/Scenes/Home/Model/ResponseModel/LikeCancelResponseModel.swift
@@ -9,11 +9,9 @@ import Foundation
 
 // MARK: - HistoryResponseModel
 struct LikeCancelResponseModel: Codable {
-    let mumentId: Int
     let likeCount: Int
 
     enum CodingKeys: String, CodingKey {
-        case mumentId
         case likeCount
     }
 }
