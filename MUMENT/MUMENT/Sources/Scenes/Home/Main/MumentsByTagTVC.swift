931926130942336cae1007e1aad0ddcbe677e388
dev-madilyn <lollin0@naver.com>
2023-02-06 19:22:48 +0900

    Chore: MumentsByTagTVC 정렬 #263

--- MUMENT/MUMENT/Sources/Scenes/Home/Main/MumentsByTagTVC.swift
+++ MUMENT/MUMENT/Sources/Scenes/Home/Main/MumentsByTagTVC.swift
@@ -82,7 +82,7 @@ extension MumentsByTagTVC: UICollectionViewDelegate{
             cell.isSelected = true
         }
         self.delegate?.mumentsByTagCVCSelected(data: mumentsByTagData[indexPath.row])
-
+        
     }
 }
 
@@ -113,7 +113,6 @@ extension MumentsByTagTVC: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-           return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 15)
-        }
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 15)
+    }
 }
-
