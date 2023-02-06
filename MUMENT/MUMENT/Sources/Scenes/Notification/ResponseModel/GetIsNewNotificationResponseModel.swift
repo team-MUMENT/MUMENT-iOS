b7f83737954235cb5c16e4c8ccc5cdd5facc11ce
dev-madilyn <lollin0@naver.com>
2023-02-07 00:05:47 +0900

    Move: Notification Response Model 폴더링 #265

---
+++ MUMENT/MUMENT/Sources/Scenes/Notification/ResponseModel/GetIsNewNotificationResponseModel.swift
@@ -0,0 +1,13 @@
+//
+//  GetIsNewNotificationResponseModel.swift
+//  MUMENT
+//
+//  Created by madilyn on 2023/02/06.
+//
+
+import Foundation
+
+// MARK: - GetIsNewNotificationResponseModel
+struct GetIsNewNotificationResponseModel: Codable {
+    var exist: Bool
+}
