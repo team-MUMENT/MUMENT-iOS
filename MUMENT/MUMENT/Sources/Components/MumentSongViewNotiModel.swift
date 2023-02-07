bf69a1220a65334337786ab6ec93c568351c6678
daminoworld <ekadls757@gmail.com>
2023-02-07 20:22:21 +0900

    Add: Add NotificationModel #275

---
+++ MUMENT/MUMENT/Sources/Components/MumentSongViewNotiModel.swift
@@ -0,0 +1,13 @@
+//
+//  MumentSongViewNotiModel.swift
+//  MUMENT
+//
+//  Created by 김담인 on 2023/02/07.
+//
+
+import Foundation
+
+struct MumentSongViewNotiModel {
+    let previousState: Bool
+    let likeCount: Int
+}
