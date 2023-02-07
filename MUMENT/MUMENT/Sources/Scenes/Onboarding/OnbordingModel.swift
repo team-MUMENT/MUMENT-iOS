d141b9b41541166801c700d6d2267075fe9a84a0
dev-madilyn <lollin0@naver.com>
2023-02-08 02:29:41 +0900

    Chore: 온보딩 Image 수정 및 UI 수정 #286

--- MUMENT/MUMENT/Sources/Scenes/Onboarding/OnbordingModel.swift
+++ MUMENT/MUMENT/Sources/Scenes/Onboarding/OnbordingModel.swift
@@ -11,18 +11,17 @@ struct OnboardingModel {
     let heading: String
     let highlightedHeading: String
     let subHeading: String
-    let backgroundImageTitle: String
-    var backgroundImage: UIImage? {
-        return UIImage(named:backgroundImageTitle)
+    let imageName: String
+    var image: UIImage? {
+        return UIImage(named: imageName)
     }
 }
 
 // MARK: - Extensions
 extension OnboardingModel {
     static var onboardingData: [OnboardingModel] = [
-        OnboardingModel(heading:"음악 감상을 손쉽게 기록해요!", highlightedHeading: "손쉽게 기록해요!", subHeading:"감상태그를 이용해 감정과\n인상적인 부분을 남겨보세요.", backgroundImageTitle:"onboarding1"),
-        OnboardingModel(heading:"다양한 감상을 발견해요!", highlightedHeading: "발견해요!", subHeading:"노래에 담긴 다채로운 이야기를\n검색 또는 추천을 통해 만나보세요.", backgroundImageTitle:"onboarding2"),
-        OnboardingModel(heading:"나만의 추억을 꺼내 봐요!", highlightedHeading: "꺼내 봐요!", subHeading:"내가 쌓은 감상과 인상 깊었던\n뮤멘트를 보관함에서 모아보세요.", backgroundImageTitle:"onboarding3")
+        OnboardingModel(heading:"음악 감상을 손쉽게 기록해요!", highlightedHeading: "손쉽게 기록해요!", subHeading:"감상태그를 이용해 감정과\n인상적인 부분을 남겨보세요.", imageName:"mument_onboarding_1"),
+        OnboardingModel(heading:"다양한 감상을 발견해요!", highlightedHeading: "발견해요!", subHeading:"노래에 담긴 다채로운 이야기를\n검색 또는 추천을 통해 만나보세요.", imageName:"mument_onboarding_2"),
+        OnboardingModel(heading:"나만의 추억을 꺼내 봐요!", highlightedHeading: "꺼내 봐요!", subHeading:"내가 쌓은 감상과 인상 깊었던\n뮤멘트를 보관함에서 모아보세요.", imageName:"mument_onboarding_3")
     ]
 }
-
