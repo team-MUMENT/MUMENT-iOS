b04813474aadb9dea657b791f6586f95c3b854d8
dev-madilyn <lollin0@naver.com>
2023-02-08 02:54:38 +0900

    Fix: rootViewController가 Onboarding으로 되어 있었던 문제 수정 #290

--- MUMENT/MUMENT/Application/SceneDelegate.swift
+++ MUMENT/MUMENT/Application/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = OnboardingVC()
+        window?.rootViewController = SplashVC()
         window?.makeKeyAndVisible()
     }
     
