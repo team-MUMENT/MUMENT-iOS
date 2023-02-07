f421ffe494dd51bf0cc40e7f3ddb1c85be2f2282
dev-madilyn <lollin0@naver.com>
2023-02-08 01:02:22 +0900

    Refactor: hideNavigationBar 메서드 BaseNC에 적용 #283

--- MUMENT/MUMENT/Global/Base/BaseNC.swift
+++ MUMENT/MUMENT/Global/Base/BaseNC.swift
@@ -11,7 +11,9 @@ class BaseNC: UINavigationController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        interactivePopGestureRecognizer?.delegate = self
+        
+        self.interactivePopGestureRecognizer?.delegate = self
+        self.hideNavigationBar()
     }
 }
 
@@ -21,5 +23,9 @@ extension BaseNC: UIGestureRecognizerDelegate {
     /// NavigationBar를 안 쓰고 UIView로 네비바를 구현할 때, 스와이프로 뒤로 가기 가능하게 함
     func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         return self.viewControllers.count > 1
+    }
+    
+    func hideNavigationBar() {
+        self.navigationBar.isHidden = true
     }
 }
