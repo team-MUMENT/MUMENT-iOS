54714cb05a65f73e682ccc61887969762d902fd1
daminoworld <ekadls757@gmail.com>
2023-02-10 00:54:37 +0900

    Chore: Remove WriteButton And Create Filtered Version EmptyView #312

--- MUMENT/MUMENT/Sources/Scenes/Storage/StorageEmptyView.swift
+++ MUMENT/MUMENT/Sources/Scenes/Storage/StorageEmptyView.swift
@@ -52,22 +52,25 @@ extension StorageEmptyView {
     }
     
     func setMyMumentLayout() {
-        self.addSubviews([titleLabel, subTitleLabel, writeButton])
+        self.addSubviews([imageView, titleLabel, subTitleLabel])
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(130.adjustedH)
+            $0.width.equalTo(135.adjustedH)
+            $0.height.equalTo(109.adjustedH)
+            $0.centerX.equalToSuperview()
+        }
         
         titleLabel.snp.makeConstraints {
-            $0.left.right.equalToSuperview()
-            $0.top.equalToSuperview().inset(170.adjustedH)
+            $0.top.equalTo(imageView.snp.bottom).offset(20)
+            $0.centerX.equalTo(imageView)
+            $0.height.equalTo(22)
         }
         
         subTitleLabel.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(8)
-            $0.leading.trailing.equalToSuperview()
-        }
-        
-        writeButton.snp.makeConstraints {
-            $0.top.equalTo(subTitleLabel.snp.bottom).offset(30.adjustedH)
-            $0.left.right.equalToSuperview().inset(88)
-            $0.height.equalTo(40)
+            $0.centerX.equalTo(titleLabel)
+            $0.height.equalTo(20)
         }
     }
     
@@ -87,6 +90,25 @@ extension StorageEmptyView {
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(imageView.snp.bottom).offset(20)
             $0.centerX.equalTo(imageView)
+            $0.height.equalTo(22)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+            $0.centerX.equalTo(titleLabel)
+            $0.height.equalTo(20)
+        }
+    }
+    
+    func setFilteredLayout() {
+        titleLabel.text = "아직 좋아요한 뮤멘트가 없어요."
+        subTitleLabel.text = "좋아요를 눌러 마음에 든 뮤멘트를 언제든 꺼내보세요."
+        
+        self.addSubviews([titleLabel, subTitleLabel])
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(212.adjustedH)
+            $0.centerX.equalToSuperview()
             $0.height.equalTo(22)
         }
         
