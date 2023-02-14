39f69f93d9838006ebca83a266fce1f99dad1bb1
dev-madilyn <lollin0@naver.com>
2023-02-15 01:37:22 +0900

    Fix: 프로필 설정 뷰에서 앞뒤 공백 제거하고 네트워킹하도록 수정 #335

--- MUMENT/MUMENT/Sources/Scenes/Mypage/SetProfile/SetProfileVC.swift
+++ MUMENT/MUMENT/Sources/Scenes/Mypage/SetProfile/SetProfileVC.swift
@@ -218,7 +218,7 @@ final class SetProfileVC: BaseVC {
             if self?.nickNameTextField.text == UserInfo.shared.nickname {
                 self?.requestSetProfile(nickname: UserInfo.shared.nickname)
             } else {
-                self?.checkDuplicatedNickname(nickname: self?.nickNameTextField.text ?? "")
+                self?.checkDuplicatedNickname(nickname: self?.nickNameTextField.clearSideEmptyText() ?? "")
             }
         }
     }
