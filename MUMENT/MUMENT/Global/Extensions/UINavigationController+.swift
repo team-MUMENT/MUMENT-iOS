d0cbf6c841b8086e7cbbdf13f7d3f6a9eeb81e67
Jimin Kim <0305jimin@gmail.com>
2023-02-24 13:02:20 +0900

    Chore: adding UINavigationController+ func #370

--- MUMENT/MUMENT/Global/Extensions/UINavigationController+.swift
+++ MUMENT/MUMENT/Global/Extensions/UINavigationController+.swift
@@ -11,4 +11,13 @@ extension UINavigationController {
     var previousViewController: UIViewController? {
         self.viewControllers.count > 1 ? self.viewControllers[self.viewControllers.count - 2] : nil
     }
+    
+    public func pushViewController(_ viewController: UIViewController,
+                                   animated: Bool,
+                                   completion: (() -> Void)?) {
+        CATransaction.begin()
+        CATransaction.setCompletionBlock(completion)
+        pushViewController(viewController, animated: animated)
+        CATransaction.commit()
+    }
 }
