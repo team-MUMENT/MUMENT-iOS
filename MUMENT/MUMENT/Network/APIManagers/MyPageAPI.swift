858909b7c34a2ca9861ed062a190b037381799c0
Jimin Kim <0305jimin@gmail.com>
2023-03-06 14:22:19 +0900

    Feat: connecting to withdrawal API #402

--- MUMENT/MUMENT/Network/APIManagers/MyPageAPI.swift
+++ MUMENT/MUMENT/Network/APIManagers/MyPageAPI.swift
@@ -44,6 +44,21 @@ class MyPageAPI: BaseAPI {
         }
     }
     
+    /// [POST] 회원 탈퇴
+    func postWithdrawal(socialToken: String, completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        AFmanager.request(MyPageService.postWithdrawal(socialToken: socialToken)).responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let data = response.data else { return }
+                let networkResult = self.judgeStatus(by: statusCode, data, WithdrawalResponseModel.self)
+                completion(networkResult)
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+    
     /// [GET] 닉네임 중복 체크
     func checkDuplicatedNickname(nickname: String, completion: @escaping (NetworkResult<Any>) -> (Void)) {
         AFmanager.request(MyPageService.checkDuplicatedNickname(nickname: nickname)).responseData { response in
